67b6ae3150ecd9440548fe217fb1272e8c05c380
gabriel <gabrielpereiralemes@gmail.com>
2023-03-02 23:16:42 -0300

    Project Sarah

---
+++ dono/menus/comandos-termux.js
@@ -0,0 +1,12 @@
+const cmd_termux = (prefix) => {
+return `
+───────────────
+
+Type ERROR: EXE 552
+      It seems that the system is not responding as expected.
+
+───────────────
+`
+}
+
+exports.cmd_termux = cmd_termux
---
+++ dono/menus/hospedar-heroku.js
@@ -0,0 +1,14 @@
+const hospedar = (prefix) => {
+return `
+
+───────────────
+
+Type ERROR: EXE 552
+      It seems that the system is not responding as expected.
+
+───────────────
+
+`
+}
+
+exports.hospedar = hospedar
